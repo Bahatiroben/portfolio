d1705da5a79c525e7dfbf7437753cd3f06baf652
BAHATI Robben <bahatiroben@gmail.com>
2020-06-02 14:24:19 +0200

    Updates

---
+++ precache-manifest.b11fbcc5f797490cdf4692e2328157b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24c96ccc1a278c84214c0daa39118f0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4d9f5b22b654719f9d43",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "/portfolio/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9f5b22b654719f9d43",
+    "url": "/portfolio/static/js/main.08e71373.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
